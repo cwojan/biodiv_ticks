c72204ef5703480b015e239519e19efdfdffba22
Kit Wojan <cwojan@gmail.com>
2025-09-04 13:39:31 -0500

    wrote up ways to analyze probability of species richness presence based on richness

--- pres_by_rich.R
+++ pres_by_rich.R
@@ -90,13 +90,23 @@ mna_by_session <- left_join(taxon_by_session, mna_summary,
 
 
 ## which species in which sites/nlcd_class
-site_nlcd_summary <- species_by_session %>%
+site_nlcd_summary <- mna_by_session %>%
   ungroup() %>%
   filter(mna > 0) %>%
   select(site_id, nlcd_class, taxon_id, mna) %>%
   group_by(site_id, nlcd_class, taxon_id) %>%
   summarize(total_mna = sum(mna)) %>%
   ungroup()
+
+## species pools for each site and nlcd
+site_nlcd_species_pools <- site_nlcd_summary %>%
+  group_by(site_id, nlcd_class) %>%
+  summarize(species_pool = list(taxon_id),
+            n_species = n(),
+            .groups = "drop")
+
+## average pool size
+ave_pool <- mean(site_nlcd_species_pool_sizes$n_species)
 
 ## create a function to randomly sample n species from the pool of species
 ## observed at a site and land cover class combination
@@ -152,3 +162,191 @@ rm(simulated_comms)
 
 ## next step run logistic regression for each species, each rep, of presence ~ richness
 ## generate distribution of effect sizes
+
+mamms <- c("PELE", "PEMA", "BLBR", "MYGA", "TAST", "NAIN")
+
+## nest and mutate method suggested by chatgpt
+
+logistic_sim_results <- simulated_comms_df %>%
+  filter(species %in% mamms) %>%
+  group_by(species, rep) %>%
+  nest() %>%
+  mutate(
+    model = map(data, ~ glm(presence ~ richness, data = ., family = binomial)),
+    intercept = map_dbl(model, ~ coef(.x)[["(Intercept)"]]),
+    coef  = map_dbl(model, ~ coef(.x)[["richness"]]),
+    type = "sim"
+  ) %>%
+  select(species, rep, coef, intercept, type)
+
+## now run the same logistic regression on the observed data
+# first correct mna_by_session to only include rows where the species is in the relevant species pool
+mna_by_session_corrected <- mna_by_session %>%
+  left_join(site_nlcd_species_pools, by = c("site_id", "nlcd_class")) %>%
+  rowwise() %>%
+  filter(taxon_id %in% species_pool) %>%
+  ungroup() %>%
+  select(-species_pool, -n_species)
+
+
+logistic_obs_results <- mna_by_session_corrected %>%
+  filter(taxon_id %in% mamms) %>%
+  group_by(taxon_id) %>%
+  nest() %>%
+  mutate(
+    model = map(data, ~ glm(presence ~ richness, data = ., family = binomial)),
+    intercept = map_dbl(model, ~ coef(.x)[["(Intercept)"]]),
+    coef  = map_dbl(model, ~ coef(.x)[["richness"]]),
+    type = "obs",
+    rep = "NA"
+  ) %>%
+  select(species = taxon_id, rep, coef, intercept, type)
+
+## bind the simulated and observed results
+logistic_sim_results <- logistic_sim_results %>%
+  group_by(species) %>%
+  mutate(rep = as.character(rep),
+         prob_at_1 = plogis(intercept + coef * 1),
+         species = as.factor(species),
+         species = fct_relevel(species, mamms))
+logistic_obs_results <- logistic_obs_results %>%
+  group_by(species) %>%
+  mutate(prob_at_1 = plogis(intercept + coef * 1),
+         species = as.factor(species),
+         species = fct_relevel(species, mamms))
+logistic_results <- bind_rows(logistic_sim_results, logistic_obs_results) %>%
+  mutate(species = as.factor(species),
+         species = fct_relevel(species, mamms))
+
+logistic_intervals <- logistic_sim_results %>%
+  group_by(species) %>%
+  summarize(lower_coef = quantile(coef, probs = 0.025),
+            upper_coef = quantile(coef, probs = 0.975),
+            lower_prob = quantile(prob_at_1, probs = 0.025),
+            upper_prob = quantile(prob_at_1, probs = 0.975),
+            .groups = "drop") %>%
+  mutate(species = as.factor(species),
+         species = fct_relevel(species, mamms))
+
+## visualize the results
+
+mamm_labels <- c(
+  PELE = "White-footed\nMouse",
+  PEMA = "Deer Mouse",
+  BLBR = "Short-tailed\nShrew",
+  MYGA = "Red-backed\nVole",
+  TAST = "Eastern\nChipmunk",
+  NAIN = "W. Jumping\nMouse"
+)
+
+ggplot(logistic_sim_results, aes(x = species, color = species, fill = species)) +
+  geom_jitter(aes(y = coef, shape = "Simulated"), alpha = 0.05,
+              width = 0.1) +
+  geom_errorbar(data = logistic_intervals, 
+                aes(ymin = lower_coef, ymax = upper_coef),
+                width = 0.2,
+                linewidth = 1) +
+  geom_point(data = logistic_obs_results,
+             aes(y = coef, shape = "Observed"), color = "black", size = 3) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  scale_shape_manual(name = "Type", values = c("Observed" = 21, "Simulated" = 16),
+                     guide = guide_legend(override.aes = list(alpha = 1, fill = "grey"))) +
+  scale_alpha_manual(values = c(1, 0.1)) +
+  scale_size_manual(values = c(2, 0.5), guide = "none") +
+  scale_color_manual(values = c("PELE" = "#E66101", "PEMA" = "#008080", "BLBR" = "#008080",
+                                      "MYGA" = "#008080", "TAST" = "#008080", "NAIN" = "#008080"),
+                     guide = "none") +
+  scale_fill_manual(values = c("PELE" = "#E66101", "PEMA" = "#008080", "BLBR" = "#008080",
+                                "MYGA" = "#008080", "TAST" = "#008080", "NAIN" = "#008080"),
+                    guide = "none") +
+  scale_x_discrete(labels = mamm_labels) +
+  scale_y_continuous(limits = c(-0.1, max(logistic_results$coef))) +
+  labs(y = "Logistic Regression Coefficient (Log Odds Ratio)", x = "Species") +
+  theme_bw() +
+  theme(legend.position = c(0.9,0.85),
+        legend.background = element_rect(fill = "white", color = "black"))
+
+ggplot(logistic_sim_results, aes(x = species, color = species, fill = species)) +
+  geom_jitter(aes(y = prob_at_1 * 100, shape = "Simulated"), alpha = 0.05,
+              width = 0.1) +
+  geom_errorbar(data = logistic_intervals, 
+                aes(ymin = lower_prob * 100, ymax = upper_prob * 100),
+                width = 0.2,
+                linewidth = 1) +
+  geom_point(data = logistic_obs_results,
+             aes(y = prob_at_1 * 100, shape = "Observed"), color = "black", size = 3) +
+  scale_shape_manual(name = "Type", values = c("Observed" = 21, "Simulated" = 16),
+                     guide = guide_legend(override.aes = list(alpha = 1, fill = "grey"))) +
+  scale_alpha_manual(values = c(1, 0.1)) +
+  scale_size_manual(values = c(2, 0.5), guide = "none") +
+  scale_color_manual(values = c("PELE" = "#E66101", "PEMA" = "#008080", "BLBR" = "#008080",
+                                "MYGA" = "#008080", "TAST" = "#008080", "NAIN" = "#008080"),
+                     guide = "none") +
+  scale_fill_manual(values = c("PELE" = "#E66101", "PEMA" = "#008080", "BLBR" = "#008080",
+                               "MYGA" = "#008080", "TAST" = "#008080", "NAIN" = "#008080"),
+                    guide = "none") +
+  scale_x_discrete(labels = mamm_labels) +
+  scale_y_continuous(labels = scales::percent_format(scale = 1),
+                     limits = c(0, 100)) +
+  labs(y = "Presence Probability at Richness = 1", x = "Species") +
+  theme_bw() +
+  theme(legend.position = c(0.9,0.85),
+        legend.background = element_rect(fill = "white", color = "black"))
+
+
+logistic_obs_preds <- mna_by_session_corrected %>%
+  filter(taxon_id %in% mamms) %>%
+  group_by(taxon_id) %>%
+  nest() %>%
+  mutate(
+    model = map(data, ~ glm(presence ~ richness, data = ., family = binomial)),
+    ## save fitted values with corresponding richness values
+    preds = map(model, ~ {
+      richness_seq <- seq(0, max(mna_by_session$richness), by = 1)
+      pred_df <- tibble(richness = richness_seq)
+      pred_df$fit <- predict(.x, newdata = pred_df, type = "response")
+      return(pred_df)
+    })
+  ) %>%
+  select(species = taxon_id, preds) %>%
+  unnest(cols = c(preds)) %>%
+  mutate(species = as.factor(species),
+         species = fct_relevel(species, mamms))
+
+
+
+reference_preds <- tibble(richness = seq(0, max(mna_by_session$richness), by = 1)) %>%
+  mutate(fit = richness / ave_pool)
+
+ggplot() +
+  geom_line(data = reference_preds,
+            aes(x = richness, y = fit * 100), color = "black", linetype = "dashed", size = 1) +
+  geom_line(data = logistic_obs_preds,
+            aes(x = richness, y = fit * 100, color = species), size = 1) +
+  scale_color_manual(values = c("PELE" = "#E66101", "PEMA" = "#ccebc5", "BLBR" = "#a8ddb5",
+                                "MYGA" = "#7bccc4", "TAST" = "#43a2ca", "NAIN" = "#0868ac"),
+                     labels = mamm_labels,
+                     name = "Species") +
+  scale_x_continuous(breaks = seq(0, max(mna_by_session$richness), by = 1)) +
+  scale_y_continuous(labels = scales::percent_format(scale = 1),
+                     limits = c(0, 100)) +
+  labs(y = "Predicted Presence Probability", x = "Species Richness") +
+  theme_bw() +
+  theme(legend.position = c(0.9,0.25),
+        legend.key.spacing.y = unit(0.2, "cm"),
+        legend.background = element_rect(fill = "white", color = "black"))
+
+## check observed presence of PELE across richness, visually
+pele_obs <- mna_by_session_corrected %>%
+  filter(taxon_id == "PELE") %>%
+  group_by(richness) %>%
+  summarize(presence_rate = mean(presence),
+            n_sessions = n(),
+            n_presences = sum(presence),
+            .groups = "drop")
+
+
+
+
+
+
