08b2d31e6ff2f45d765026295d7530ff3c9b7ebf
Kit Wojan <cwojan@gmail.com>
2025-09-09 11:39:23 -0500

    better tick "preference" analysis, but not enough power

--- tick_attachment.R
+++ tick_attachment.R
@@ -190,8 +190,86 @@ ggplot(tick_by_rich_preds_filt, aes(x = richness, y = fit, color = taxon_id)) +
         legend.key.spacing.y = unit(0.2, "cm"),
         legend.background = element_rect(fill = "white", color = "black"))
 
+## tick "preference" analysis
 
-## ticks on pele analysis
+# first let's check if the total mna column is accurate for mammals with tick info
+mna_check <- mamm_tick_comm %>%
+  group_by(plot_session) %>%
+  summarize(total_mna_check = n(),
+            total_mna_reported = first(total_mna),
+            .groups = "drop") %>%
+  filter(total_mna_check != total_mna_reported)
+
+## many mismatches, so make a new total mna column based on mammal ticks data
+mamm_tick_pref <- mamm_tick_comm %>%
+  group_by(plot_session) %>%
+  mutate(total_tick_mna = n()) %>%
+  group_by(plot_session, taxon_id) %>%
+  mutate(mna_tick = n(),
+         prop_mna_tick = mna_tick / total_tick_mna,
+         n_w_ticks = sum(ticks == 1),
+         total_w_ticks = sum(ticks),
+         prop_w_ticks = n_w_ticks / total_w_ticks,
+         tick_pref = prop_w_ticks / prop_mna_tick) %>%
+  filter(total_w_ticks > 0) %>%
+  ungroup()
+
+tick_pref_summary <- mamm_tick_pref %>%
+  group_by(taxon_id) %>%
+  summarize(mean_tick_pref = mean(tick_pref, na.rm = TRUE),
+            upper = quantile(tick_pref, 0.975, na.rm = TRUE),
+            lower = quantile(tick_pref, 0.025, na.rm = TRUE),
+            n = n(),
+            .groups = "drop") %>%
+  mutate(taxon_id = as.factor(taxon_id),
+         taxon_id = fct_relevel(taxon_id, mamms))
+
+ggplot(tick_pref_summary, aes(x = taxon_id, y = mean_tick_pref, color = taxon_id)) +
+  geom_point(size = 4) +
+  geom_errorbar(aes(ymin = lower, ymax = upper), width = 0.1) +
+  scale_color_manual(values = c("PELE" = "#E66101", "PEMA" = "#008080", "BLBR" = "#008080",
+                                "MYGA" = "#008080", "TAST" = "#008080", "NAIN" = "#008080"),
+                     guide = "none") +
+  scale_x_discrete(labels = mamm_labels) +
+  scale_y_continuous(limits = c(0, max(tick_pref_summary$upper)), breaks = seq(0, max(tick_pref_summary$upper), by = 1)) +
+  labs(x = "Species", y = "Mean Tick Preference Index") +
+  theme_bw() +
+  theme(axis.title = element_text(size = 24),
+        axis.text = element_text(size = 16))
+
+tick_pref_sum_by_rich <- mamm_tick_pref %>%
+  group_by(taxon_id, richness) %>%
+  summarize(mean_tick_pref = mean(tick_pref, na.rm = TRUE),
+            upper = quantile(tick_pref, 0.975, na.rm = TRUE),
+            lower = quantile(tick_pref, 0.025, na.rm = TRUE),
+            n = n(),
+            .groups = "drop") %>%
+  mutate(taxon_id = as.factor(taxon_id),
+         taxon_id = fct_relevel(taxon_id, mamms))
+
+ggplot(tick_pref_sum_by_rich, aes(x = richness, y = mean_tick_pref, color = taxon_id)) +
+  geom_line(size = 1) +
+  geom_ribbon(aes(ymin = lower, ymax = upper, fill = taxon_id), alpha = 0.2, color = NA) +
+  scale_color_manual(values = c("PELE" = "#E66101", "PEMA" = "#ccebc5", "BLBR" = "#a8ddb5",
+                                "MYGA" = "#7bccc4", "TAST" = "#43a2ca", "NAIN" = "#0868ac"),
+                     labels = mamm_labels,
+                     name = "Species") +
+  scale_fill_manual(values = c("PELE" = "#E66101", "PEMA" = "#ccebc5", "BLBR" = "#a8ddb5",
+                               "MYGA" = "#7bccc4", "TAST" = "#43a2ca", "NAIN" = "#0868ac"),
+                    guide = "none") +
+  scale_x_continuous(breaks = seq(0, 10, by = 1), limits = c(1, max(mamm_tick_comm$richness))) +
+  scale_y_continuous(limits = c(0, max(tick_pref_sum_by_rich$upper)), breaks = seq(0, max(tick_pref_sum_by_rich$upper), by = 1)) +
+  labs(x = "Small Mammal Species Richness", y = "Mean Tick Preference Index") +
+  theme_bw() +
+  theme(axis.title = element_text(size = 24),
+        axis.text = element_text(size = 16),
+        legend.title = element_text(size = 20),
+        legend.text = element_text(size = 16),
+        legend.position = "bottom",
+        legend.key.spacing.y = unit(0.2, "cm"),
+        legend.background = element_rect(fill = "white", color = "black"))
+
+## ticks on pele analysis (bad method)
 
 ticks_on_pele <- mamm_tick_comm %>%
   group_by(plot_session) %>%
